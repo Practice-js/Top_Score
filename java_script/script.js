cf9cdafee5f33c3a564d2dd417a4210a02d3c075
Nazanin sharifi | نازنین شریفی <143528080+mikaela4518@users.noreply.github.com>
2023-12-23 15:43:32 +0330

    Creating a data structure of students of a school
    +Defining the array [ school ] and defining its classes in the form of objects to store the characteristics of each class and students' information
    
    +Writing the birth certificate of the JavaScript file

---
+++ java_script/script.js
@@ -0,0 +1,52 @@
+/* Birth certificate of this JS
+
+name project : top score in school
+---------------------------------------------------
+date : 2023 22 Dec
+---------------------------------------------------
+team developers : nazanin sharifi , tahereh safary
+---------------------------------------------------
+contant wiht gmail : mikaelasharifi45@gmail.com
+
+*/
+let school = [
+    // class 1
+    {
+        info: {
+            totalStudent: 4,
+            teacherNeam: "khateri",
+            classNeme: "Web Design"
+        },
+
+        students: [
+            // std 1
+            {
+                name: "nazanin",
+                score: 20,
+                codeMali: 3550210150
+            },
+            // std 2
+            {
+                name: "tahereh",
+                score: 19,
+                codeMali: 63948574617
+
+            },
+            // std 3
+            {
+                name: "raheleh",
+                score: 19.5,
+                codeMali: 9592686562
+            },
+            // sdt 4
+            {
+                name: "maedeh",
+                score: 16,
+                codeMali: 3965487215
+            }
+        ]
+    },
+    // class 2
+    {}
+]
+
